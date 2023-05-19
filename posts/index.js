ad920c93e9753e1e32b1a23c7c700325049df8fd
lehuuhieu-0805 <01643815296h@gmail.com>
2023-05-19 15:25:52 +0700

    feat: get all and create a post

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = { id, title };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
