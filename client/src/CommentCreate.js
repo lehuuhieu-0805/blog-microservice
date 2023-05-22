300bf3ef5a57096a344de29340d638aab6880bc4
lehuuhieu-0805 <01643815296h@gmail.com>
2023-05-22 18:37:46 +0700

    create comment - FE

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+function CommentCreate({ postId }) {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios({
+      method: 'POST',
+      url: `http://localhost:4001/posts/${postId}/comments`,
+      data: {
+        content,
+      },
+    });
+
+    setContent('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default CommentCreate;
