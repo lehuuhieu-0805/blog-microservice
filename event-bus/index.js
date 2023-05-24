3d572bc4a39f95ea6cfdb16d6256a4916fa089f7
lehuuhieu-0805 <01643815296h@gmail.com>
2023-05-24 18:35:16 +0700

    feat: build event bus

---
+++ event-bus/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 4005;
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  axios({
+    method: 'POST',
+    url: 'http://localhost:4000/events',
+    data: {
+      type,
+      data,
+    },
+  });
+  axios({
+    method: 'POST',
+    url: 'http://localhost:4001/events',
+    data: {
+      type,
+      data,
+    },
+  });
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
