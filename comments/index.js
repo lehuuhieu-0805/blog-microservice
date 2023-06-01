afcee919c9eca3ae57bf536e1e98c011ae345441
lehuuhieu-0805 <01643815296h@gmail.com>
2023-06-01 14:32:26 +0700

    feat: emit comment update event to event bus service

--- comments/index.js
+++ comments/index.js
@@ -10,6 +10,12 @@ app.use(cors());
 const PORT = process.env.PORT || 4001;
 const commentsByPostId = {};
 
+const EVENT_TYPE = {
+  COMMENT_CREATED: 'CommentCreated',
+  COMMENT_MODERATED: 'CommentModerated',
+  COMMENT_UPDATED: 'CommentUpdated',
+};
+
 app.get('/posts/:id/comments', (req, res) => {
   // id post
   const { id } = req.params;
@@ -32,7 +38,7 @@ app.post('/posts/:id/comments', async (req, res) => {
     method: 'POST',
     url: 'http://localhost:4005/events',
     data: {
-      type: 'CommentCreated',
+      type: EVENT_TYPE.COMMENT_CREATED,
       data: {
         id: commentId,
         content,
@@ -45,8 +51,33 @@ app.post('/posts/:id/comments', async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event: ', req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === EVENT_TYPE.COMMENT_MODERATED) {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios({
+      method: 'POST',
+      url: 'http://localhost:4005/events',
+      data: {
+        type: EVENT_TYPE.COMMENT_UPDATED,
+        data: {
+          id,
+          status,
+          content,
+          postId,
+        },
+      },
+    });
+  }
 
   res.send({});
 });
