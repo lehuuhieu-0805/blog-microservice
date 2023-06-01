48fb80662594c94800f4b55a5685a1c4867c40a7
lehuuhieu-0805 <01643815296h@gmail.com>
2023-06-01 14:10:48 +0700

    feat: moderation comment

--- moderation/index.js
+++ moderation/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,7 +8,29 @@ app.use(cors());
 
 const PORT = process.env.PORT || 4003;
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios({
+      method: 'POST',
+      url: 'http://localhost:4005/events',
+      data: {
+        type: 'CommentModerated',
+        data: {
+          id: data.id,
+          postId: data.postId,
+          status,
+          content: data.content,
+        },
+      },
+    });
+  }
+
+  res.send({});
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
